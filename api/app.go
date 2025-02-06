171622c63f9a246c3075fbcc8a82010c699bbfcf
rifrifqyqy <rifqyh22@gmail.com>
2025-02-07 03:43:55 +0700

    chore: init

--- api/app.go
+++ api/app.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
+// Handler untuk menjalankan aplikasi Fiber
+func Handler(w http.ResponseWriter, r *http.Request) {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, Vercel!")
     })
 
-    app.Listen(":3000")
+    // Menjalankan Fiber aplikasi pada HTTP handler
+    app.Handler()
+}
+
+// Main function harus tetap ada untuk Vercel
+func main() {
+    http.HandleFunc("/", Handler)
+    http.ListenAndServe(":3000", nil)
 }
